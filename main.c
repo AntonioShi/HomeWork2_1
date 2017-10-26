d1ed6a000551c423f321bb8ae6fb314c927f82b3
Antonio Shi <syc694482337@outlook.com>
2017-10-26 19:22:47 +0800

    /**2-1單鏈表插入  * 编写一个在单链表中数据域值为a的结点之后，  * 插入一个新结点的算法，如果链表中没有数据域值为a的结点，  * 则把新结点插入到表尾.  */

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
  * 插入一个新结点的算法，如果链表中没有数据域值为a的结点，
  * 则把新结点插入到表尾.
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
@@ -32,27 +33,26 @@ int ListLength(SLNode *head){
 }
 /*******************************************************/
 //在數據與爲a的節點後插入新節點,考慮異常情況
-//("抱歉沒有找到數據與爲a的節點,那麼就默認插到連尾") ;
-int ListInsert(SLNode *head, DataType a){
+int ListInsert(SLNode *head, int a, DataType s){
     SLNode *p, *q ;
     p = head ;
     while (p->next != NULL && p->data != a){
         p = p->next ;
     }
     if(p->next == NULL && p->data != a){
-
+        printf("抱歉沒有找到數據與爲a的節點,那麼就默認插到連尾") ;
         q = (SLNode *)malloc(sizeof(SLNode)) ;
         p->next = q ;
         q->next = NULL ;
-        q->data = a ;
-        //printf("尾插入成功\n") ;
+        q->data = s ;
+        printf("尾插入成功") ;
         return 1 ;
     }
     else{//找到了
 
         q = (SLNode *)malloc(sizeof(SLNode)) ;
         q->next = p->next ;
-        q->data = a ;
+        q->data = s ;
         p->next = q ;
         return 1;
     }
@@ -60,9 +60,9 @@ int ListInsert(SLNode *head, DataType a){
 /*******************************************************/
 //輸入現有鏈表裏面的數據
 void ListPrint(SLNode *head){
-    SLNode *p = head->next ;
-    while(p != NULL){
-        printf("%-4d", p->data) ;
+    SLNode *p = head ;
+    while(p->next != NULL){
+        printf("  %d", p->data) ;
         p = p->next ;
     }
 }
@@ -72,14 +72,9 @@ int main() {
     SLNode *head ;
     ListInitiate(&head) ;
     for (int i = 0; i < 10; ++i) {
-        ListInsert(head, i+1) ;
+        ListInsert(head, i, i+1) ;
     }
 
-    int index = 7 ;
-    int non = 20 ;
-
-    ListInsert(head, index) ;//验证第一种情况
-    ListInsert(head, non) ;//验证第二种情况
     ListPrint(head) ;
     return 0;
 }
